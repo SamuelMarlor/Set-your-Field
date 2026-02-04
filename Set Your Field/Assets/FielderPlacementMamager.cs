555ca7edfe56e15cb1ae0d3d214c0da7df583f70
SamuelMarlor <23652403@stu.mmu.ac.uk>
2026-02-04 17:56:34 +0000

    shadow

--- Set Your Field/Assets/FielderPlacementMamager.cs
+++ Set Your Field/Assets/FielderPlacementMamager.cs
@@ -23,7 +23,7 @@ public class FielderPlacementManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        fieldersRemainingText.text = "" + fieldersLeft;
+        
     }
 
 
