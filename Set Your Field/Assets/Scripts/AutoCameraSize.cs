852608b2cb3b329f41cdefc88b4e3162e6845e62
SamuelMarlor <23652403@stu.mmu.ac.uk>
2026-03-16 22:57:57 +0000

    save

---
+++ Set Your Field/Assets/Scripts/AutoCameraSize.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AutoCameraSize : MonoBehaviour
+{
+    public float referenceWidth = 1920f;
+    public float referenceHeight = 1080f;
+
+    void Start()
+    {
+        float targetAspect = referenceWidth / referenceHeight;
+        float windowAspect = (float)Screen.width / Screen.height;
+
+        Camera cam = GetComponent<Camera>();
+
+        if (windowAspect < targetAspect)
+        {
+            // Taller screen → zoom OUT
+            float difference = targetAspect / windowAspect;
+            cam.orthographicSize *= difference;
+        }
+    }
+}
